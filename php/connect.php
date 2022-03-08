1aae9a7ef20c7a211550a46a45d1bbc41c2aafa1
manal magdy <manalmagdy694@gmail.com>
2022-03-09 05:09:01 +0800

    .

--- php/connect.php
+++ php/connect.php
@@ -7,13 +7,21 @@ $conn = new mysqli('localhost','root' ,'','frontSteps');
 if($conn->connect_error){
     die('Connection Failed :'.$conn->connect_error);
 }else{
-    $stmt = $conn->prepare("insert into registration(fullname,ID,income,doc)
+    $sql = "SELECT * FROM  registration where ID ='".$_POST['ID']."'";  
+    $resultset_1=mysqli_query($conn, $sql);  
+    $count = mysqli_num_rows($resultset_1);  
+    if($count == 0){
+        $stmt = $conn->prepare("insert into registration(fullname,ID,income,doc)
         values(?,?,?,?)");
         $stmt->bind_param("ssib",$fullname , $ID,$income,$doc);
         $stmt->execute();
         echo "applicant registered successfully...";
         $stmt->close();
         $conn->close();
+    }else{
+        echo "The user ID already has been registered" ;
+    }
+
 }
 ?>
 
