2ec0b4cbd6ff8242c8cfdcd6b33e9afb7c183d5e
manal magdy <manalmagdy694@gmail.com>
2022-03-09 04:10:45 +0800

    .

--- php/connect.php
+++ php/connect.php
@@ -6,6 +6,14 @@ $doc = $_POST['doc'];
 $conn = new mysqli('localhost','root' ,'','frontSteps');
 if($conn->connect_error){
     die('Connection Failed :'.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into registration(fullname,ID,income,doc)
+        values(?,?,?,?)");
+        $stmt->bind_param("ssib",$fullname , $ID,$income,$doc);
+        $stmt->execute();
+        echo "applicant registered successfully...";
+        $stmt->close();
+        $conn->close();
 }
 ?>
 
